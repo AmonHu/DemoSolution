d4eaa613e97a0b33f2d52fbcb2ce2e5d788875ce
AmonHu <amonhu@outlook.com>
2019-09-17 22:31:44 +0800

    添加项目文件。

---
+++ UdpCilentDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpCilentDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                using (var udpClient = new UdpClient(1399))
+                {
+                    while (true)
+                    {
+                        //IPEndPoint object will allow us to read datagrams sent from any source.
+                        var result = await udpClient.ReceiveAsync();
+                        Console.WriteLine(Encoding.ASCII.GetString(result.Buffer));
+
+                    }
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
